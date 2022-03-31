3021956e5b14f2ff9365b65d4b9b3ea3177f5762
Dario Tinoco <64984354+dario-t@users.noreply.github.com>
2022-03-31 16:29:54 +0200

    Add files via upload

--- chatbot_meta.py
+++ chatbot_meta.py
@@ -1,36 +1,44 @@
-from urllib.request import urlopen
 import re
 import json
 from datetime import datetime
 import random
-from googlesearch import search
 import pandas as pd
+import webbrowser
+from recomend_sys import *
+import os
+from sys import platform
 
-dir = "C:\\Users\\Ich\\OneDrive\\python\\Proyect\\Meta_chat_bot\\intents.json"
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-with open(dir, "r", encoding="utf-8") as json_file:
-    json_entry = json.load(json_file)
+if platform == "win32":
+    dir = base_dir + r"json\intents.json"
+else:
+    dir = base_dir + "json/intents.json"
+
+with open(dir, "r", encoding="utf-8") as j:
+    json_entry = json.load(j)
 
 
 # return a greeting message
-now = datetime.now()
-current_time = int(now.strftime("%H"))
-if current_time >= 5 and current_time < 11:
-    good = "Good Morning"
-elif current_time >= 11 and current_time < 15:
-    good = "Good Afternoon"
-elif current_time >= 15 and current_time < 00:
-    good = "Goof Evening"
-else:
-    good = "Good Night"
+def greeting():
+    now = datetime.now()
+    current_time = int(now.strftime("%H"))
+    if current_time >= 5 and current_time < 11:
+        good = "Good Morning"
+    elif current_time >= 11 and current_time < 15:
+        good = "Good Afternoon"
+    elif current_time >= 15 and current_time < 00:
+        good = "Good Evening"
+    else:
+        good = "Good Night"
+    return good
 
 # import df
 
-dir_games = "C:\\Users\\Ich\\OneDrive\\python\\Proyect\\Meta_chat_bot\\json\\games.json"
+dir_games = r"json\games.json"
 
 with open(dir_games,"r", encoding="utf-8") as json_file:
     data = json.load(json_file)
-    print(data)
 
 df = pd.DataFrame(data)
 
@@ -64,125 +72,188 @@ def match_games(game, list_g):
 
 list_games = [i.lower() for i in df]
 
+def search_google(input):
+    # search in google
+    search = input.replace(" ", "+")
+    webbrowser.open(search)
 
-def like_videogames(list_fav):
+
+
+def like_videogames(list_): 
+
+    global list_fav
+    list_fav = list_ # make a global variable the argument
     # conversation if like videogames
-    sent_game = input(random.choice(json_entry["intents"][3]["responses"])) # we ask about the game
+    yield random.choice(json_entry["intents"][3]["responses"]) # we ask about the game
+    sent_game = yield
     g = match_games(sent_game.lower(), list_games) # we pass the match function
  
-    if type(g)== str:  # if match 100%
-        list_fav.append(g) # append in in fav list 
-        print(random.choice(json_entry["intents"][8]["responses"]).format(g)) # print the games
+    if type(g)== str:  # if match 100% #TODO --> loop again
+        list_fav.append(g) # append in fav list 
+        yield random.choice(json_entry["intents"][8]["responses"]).format(g) # print the games
     else:
-        print(random.choice(json_entry["intents"][6]["responses"])) # if not match
+        list_g = ""
+        choos_ = random.choice(json_entry["intents"][7]["responses"]) # ask number
         for cont, value in enumerate(g[:10]):
-            print(cont, value[2]) # become a enumerate list
-         
+            list_g = "\t" + list_g + str(cont) + "-" + value[2] + "\n" # become a enumerate list
+        yield random.choice(json_entry["intents"][6]["responses"]) +"\n" + list_g + "\n" + choos_
+        g_entry = yield
         while True:
-            g_entry = input(random.choice(json_entry["intents"][7]["responses"])) # ask for the number
+             # ask for the number
             try: 
                 g_entry = int(g_entry)
-                if g_entry >= 0 and g_entry <=9: 
-                    list_fav.append(g[g_entry][2]) # if become a nummber, append in the list
-                    print(random.choice(json_entry["intents"][8]["responses"]).format(g[g_entry][2])) # print the games
-                    re_ask = input(random.choice(json_entry["intents"][9]["responses"])) # add another game?
+                if g_entry >= 0 and g_entry <=9:
+                    list_fav.append(g[g_entry][2]) # if become a number, append in the list
+                    yield f"""{random.choice(json_entry["intents"][8]["responses"]).format(g[g_entry][2])}
+{random.choice(json_entry["intents"][9]["responses"])}""" # print the games
+                    
+                    re_ask = yield # add another game?
                     if re_ask.lower() in json_entry["intents"][1]["patterns"]: # if not:
-                        print(random.choice(json_entry["intents"][10]["responses"])) # let´s go
-                        return list_fav
                         break
                     elif re_ask.lower() in json_entry["intents"][3]["patterns"]: # if add another games
-                        like_videogames(list_fav) # stat again
+                        x = like_videogames(list_fav) # start again
+                        yield from x # stat again
                         break
                     else: 
                         re_ask = (random.choice(json_entry["intents"][19]["responses"])) #TODO # sorry i don´t undertand you, stat again?
                         if re_ask in json_entry["intents"][3]["patterns"]: # if yes
-                            like_videogames(list_fav) # stat again
-                            break
+                             y = like_videogames(list_fav) # stat again
+                             yield from y # stat again
                         elif re_ask in json_entry["intents"][1]["patterns"]: 
-                            print(random.choice(json_entry["intents"][10]["responses"])) # let´s go
-                            return list_fav
-                            break                         
+                            break
 
                 else:
-                    print(random.choice(json_entry["intents"][16]["responses"])) # must to be a num between 0 and 9
+                    yield random.choice(json_entry["intents"][16]["responses"]) # must to be a num between 0 and 9
             except:
-                if g_entry.lower() == "no": # no t in list                    print(random.choice(json_entry["intents"][12]["responses"])) # show you another list
+                if g_entry.lower() == "no": # 
+                    list_g = ""
                     for cont, value in enumerate(g[10:20]):           
-                        print(cont, value[2]) # print the list
-                    re_ask_2 = input(random.choice(json_entry["intents"][13]["responses"]))# in list if not say no
+                        list_g = "\t" + list_g + str(cont) + "-" + value[2] + "\n" # become a enumerate list
+                    yield list_g + random.choice(json_entry["intents"][13]["responses"])# in list if not say no
+                    re_ask_2 = yield
+
                     if re_ask_2 in json_entry["intents"][1]["patterns"]: # if not
-                        re_ask_3 = input(random.choice(json_entry["intents"][15]["responses"])) # sorry, do you want to add another?
-                        if re_ask_3 in json_entry["intents"][1]["patterns"]: # if in no
-                            print(random.choice(json_entry["intents"][10]["responses"])) # let´s go!
-                            return list_fav
+                        yield random.choice(json_entry["intents"][15]["responses"])
+                        re_ask_3 = yield # sorry, do you want to add another?
+                        if re_ask_3 in json_entry["intents"][1]["patterns"]: # if no
                             break
+                            # yield random.choice(json_entry["intents"][10]["responses"]) # let´s go
                         elif re_ask_3 in json_entry["intents"][3]["patterns"]: # if yes
-                            like_videogames(list_fav) # stat again
+                            x = like_videogames(list_fav) # start again
+                            yield from x # stat again
                             break
                     else:
                         try:
                             re_ask_2 = int(re_ask_2) # if is a number
                             if re_ask_2 >= 0 and re_ask_2 <=9:
                                 list_fav.append(g[re_ask_2+10][2]) # add the game to the list
-                                print(random.choice(json_entry["intents"][8]["responses"]).format(g[re_ask_2+10][2])) # print the game
-                                re_ask = input(random.choice(json_entry["intents"][9]["responses"])) # another games?
+                                yield f"""{random.choice(json_entry["intents"][8]["responses"]).format(g[re_ask_2+10][2])}
+{random.choice(json_entry["intents"][9]["responses"])}""" # another games?
+                                re_ask =  yield
                                 if re_ask.lower() in json_entry["intents"][1]["patterns"]: # if not
-                                    print(random.choice(json_entry["intents"][10]["responses"])) # let´s go
-                                    return list_fav
                                     break
+
                                 elif re_ask.lower() in json_entry["intents"][3]["patterns"]: # if add another games
-                                    like_videogames(list_fav)
+                                    x = like_videogames(list_fav) # start again
+                                    yield from x # stat again
+                                    break
                                 
                                 else:
                                     re_ask = (random.choice(json_entry["intents"][19]["responses"])) #TODO # sorry i don´t undertand you, stat again?
                                     if re_ask in json_entry["intents"][3]["patterns"]: # if yes
-                                        like_videogames(list_fav) # stat again
+                                        x = like_videogames(list_fav) # start again
+                                        yield from x # stat again
                                         break
                                     elif re_ask in json_entry["intents"][1]["patterns"]: 
-                                        print(random.choice(json_entry["intents"][10]["responses"])) # let´s go
-                                        return list_fav
-                                        break                         
+                                        break
+                                        # yield random.choice(json_entry["intents"][10]["responses"]) # let´s go
                                     break
                             else:
-                                print(random.choice(json_entry["intents"][16]["responses"])) # must to be a num between 9 and 19
+                                yield random.choice(json_entry["intents"][16]["responses"]) # must to be a num between 9 and 19
                                 break
                                 
                         except: # not a number
-                            print(random.choice(json_entry["intents"][18]["responses"])) # neither nummber nor negation, start over.
+                            yield random.choice(json_entry["intents"][18]["responses"]) # neither number nor negation, start over.
                             break
+
 
 def bot():
     # main function
-    y = yield random.choice(json_entry["intents"][0]["responses"]).format(good)
-    y # ask if you like videogames 
+    # _______________________________
+    # play a sound
+    # playsound("C:\\Users\\Ich\\OneDrive\\python\\Proyect\\Meta_chat_bot\\Im Bender.mp3")
+    # ask if you like videogames 
+    yield random.choice(json_entry["intents"][0]["responses"]).format(greeting())
+    
     x = 0
     while True: 
         r = yield
-        if r.lower() in json_entry["intents"][1]["patterns"]: sent_1 = 1 # si es negativo
-        elif r.lower() in json_entry["intents"][3]["patterns"]: sent_1 = 3 # si es afirmativo
-        else: sent_1 = 4
+        if r.lower() in json_entry["intents"][1]["patterns"]: sent_1 = 1 # if yes
+        elif r.lower() in json_entry["intents"][3]["patterns"]: sent_1 = 3 # if no
+        else: sent_1 = 4 # if not understand
 
         # don´t like videogames
         if sent_1 == 1:
-            sent_1_togoogle = input(json_entry["intents"][sent_1]["responses"])
-            print(json_entry["intents"][2]["responses"][0].format(sent_1_togoogle))
-            for i in search(sent_1_togoogle, stop = 1):
-                print(i)
-            print(json_entry["intents"][14]["responses"][0])
+            r = yield random.choice(json_entry["intents"][sent_1]["responses"])
+
+        # looking in google
+            r_1 = yield 
+            yield f"Then do you like {r_1}?"
+            webbrowser.open("https://www.google.com/search?q=" + r_1)
             break
             
         # Like videogames
         elif sent_1 == 3: 
             fav_games = [] 
-            like_videogames(fav_games)
-            return fav_games
-            break 
-        
+            y = like_videogames(fav_games)
+            # while True:
+            yield from y
+            df_ = load_json()
+            media_df, games_df = data_games(df_)
+
+            #  Recommended list of games sorted by correlation.
+            a = load_json()
+            media_df, games_df = data_games(a)
+
+            y = corr_games(games_df, fav_games)
+            output_str = "Ok, here is a list of games that you might like to play \n"
+            output_str += f'\n\tPos,\tAffinity\t\t Game\n\t{"_"*60}'
+            for i in range(10):
+                output_str += f'\n\t{i}\t {round(y["mean"][i], 4)}\t\t -{y.index[i]}'
+            output_str += "\n\nNow choose a game from the list for more information"
+            yield output_str
+            info_num = yield # choose a game from the list
+
+            # Show info about the game
+            info_num = int(info_num)
+            if info_num >= 0 and info_num <= 10:
+                df = load_json()
+                df_game = df[y.index[info_num]] # get the game
+                yield f"""Ok, here is the information about\n\n{y.index[info_num]}
+Platform\t{df_game["platform"]}
+
+Date\t{df_game["date"]}
+
+Develop.\t{df_game["developer"]}
+
+Geners\t{df_game["geners"]}
+
+Rating{df_game["metascore"]}
+
+Description\t{df_game["description"]}
+                
+If you want more info, just let me know"""
+                finla_input = yield
+                if finla_input.lower() in json_entry["intents"][3]["patterns"]:
+                    webbrowser.open(df_game['url'])
+
+
+                break
+                
+                        
         else: 
             x += 1
-            if x == 2: 
-                print("ok, you are very funny, Hasta la vista")
+            if x == 2:
+                yield "ok, you are very funny, Hasta la vista"
                 break
-            sent_0 = input(random.choice(json_entry["intents"][sent_1]["responses"]))
-# y = bot()
 
