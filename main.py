f7189813546fbab2455cd54693d683b112dca261
Dario Tinoco <64984354+dario-t@users.noreply.github.com>
2022-03-31 16:29:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
-
 from chatbot_meta import *
-import tkinter
 from tkinter import *
 
 res = bot()
 def send():
     # get the message text
-    msg = EntryBox.get("1.0",'end-1c').strip()
+    msg = EntryBox.get("1.0",'end-1c')
     EntryBox.delete("0.0",END)
-    
-    ChatLog.config(state=NORMAL)
-    ChatLog.insert(END, "You: " + msg + '\n\n')
-    ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
-    nbot = next(res)
-    ChatLog.insert(END, "Bot: " + nbot + '\n\n')
-    ChatLog.config(state=DISABLED)
-    ChatLog.yview(END)
+    Chat.config(state=NORMAL)
+    Chat.insert(END, "You: " + msg + '\n\n')
+    Chat.config(foreground="#442265", font=("Verdana", 12 ))
+
+    nbot = res.send(None)   
+    try: 
+        Chat.insert(END, "Bender: " + nbot + '\n\n', )
+    except: 
+        nbot = res.send(msg)
+        Chat.insert(END, "Bender: " + nbot +  '\n\n', )
+    Chat.config(state=DISABLED)
+    Chat.yview(END)
 
 
-base = Tk()
-base.title("ChatBot - SL")
-base.geometry("400x500")
+base = Tk() # Create a window
+base.iconbitmap(r"\bender.ico")
+base.title("Bender ChatBot")
+base.geometry("800x500") # Size of the window
 base.resizable(width=FALSE, height=FALSE)
 
 #Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
-ChatLog.config(state=DISABLED)
+Chat = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+Chat.config(state=DISABLED)
 
 #Bind scrollbar to Chat window
-scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-ChatLog['yscrollcommand'] = scrollbar.set
+scrollbar = Scrollbar(base, command=Chat.yview)
+Chat['yscrollcommand'] = scrollbar.set
 
 #Create Button to send message
 SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
@@ -39,12 +42,10 @@ SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", h
 #Create the box to enter message
 EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
 
-
-
 #Place all components on the screen
-scrollbar.place(x=376,y=6, height=386)
-ChatLog.place(x=6,y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
+scrollbar.place(x=780,y=6, height=386)
+Chat.place(x=6,y=6, height=386, width=775)
+EntryBox.place(x=128, y=401, height=90, width=665)
 SendButton.place(x=6, y=401, height=90)
 
 base.mainloop()
